7d29c68699b15fb04fd29fd80560bd80da46b582
Maestro3733 <extremewinds@gmail.com>
2020-07-21 22:29:11 +0200

    add signup layout and controller

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -1,7 +1,11 @@
 package com.userfront.controller;
 
+import com.userfront.domain.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -14,5 +18,17 @@ public class HomeController {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "signup";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public void signupPost(@ModelAttribute("user") User user, Model model) {
+
     }
 }
