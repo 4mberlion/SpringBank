84d30ad14af54136716a385fa55f9cbd8325b004
Maestro3733 <extremewinds@gmail.com>
2020-07-23 16:53:17 +0200

    Handle user roles with spring security

--- src/main/java/com/userfront/service/UserSeviceImpl/UserSecurityService.java
+++ src/main/java/com/userfront/service/UserSeviceImpl/UserSecurityService.java
@@ -1,6 +1,6 @@
 package com.userfront.service.UserSeviceImpl;
 
-import com.userfront.Dao.UserDao;
+import com.userfront.dao.UserDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
